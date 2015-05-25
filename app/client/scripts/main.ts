0d508c662f75000523cd5eac846ee7a8d231b255
Jim Schubert <james.schubert@gmail.com>
2015-05-24 23:43:33 -0400

    Initial commit

---
+++ app/client/scripts/main.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata';
+import {Component, View, bootstrap} from 'angular2/angular2';
+@Component({
+  selector: 'my-app'
+})
+@View({
+  template: `<h1>My first Angular 2 App</h1>`
+})
+class AppComponent {
+}
+bootstrap(AppComponent);
---
+++ readme.md
@@ -0,0 +1,27 @@
+# electron-angular
+
+> My splendid app
+
+
+## Dev
+
+```
+$ npm install
+```
+
+### Run
+
+```
+$ npm start
+```
+
+### Build
+
+```
+$ npm run build
+```
+
+
+## License
+
+MIT © [Jim Schubert](http://ipreferjim.com)
